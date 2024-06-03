889eeb043bad6fe0dfdaba700a940e47646cb515
Bubullebizarre1981 <loic.jochmans@gmail.com>
2024-06-03 14:43:05 +0200

    Add all views

---
+++ WearRU/Views/ExplorerSearchModalView.swift
@@ -0,0 +1,18 @@
+//
+//  ExplorerSearchModalView.swift
+//  WearRU
+//
+//  Created by Apprenant 171 on 03/06/2024.
+//
+
+import SwiftUI
+
+struct ExplorerSearchModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExplorerSearchModalView()
+}
