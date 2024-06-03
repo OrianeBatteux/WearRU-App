7c8f549733681c8c17cadc6609173decde26ea25
Johan Q. <johan.quille@protojo.fr>
2024-06-03 11:32:26 +0200

    Updates MVVM

---
+++ WearRU/Utilities/Color+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Extensions.swift
+//  WearRU
+//
+//  Created by Apprenant 154 on 03/06/2024.
+//
+
+import SwiftUI
+
+extension Color {
+    static let primaryColor = Color("PrimaryColor")
+    static let secondaryColor = Color("SecondaryColor")
+    
+    struct Background {
+        static let light = Color("LightBackground")
+        static let dark = Color("DarkBackground")
+    }
+}
