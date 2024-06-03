0ce4f2c59bbe6d3e69c2017630ba9b508ed68a81
Johan Q <johan.quille@protojo.fr>
2024-06-03 10:21:45 +0200

    Initial Commit

---
+++ WearRU/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  WearRU
+//
+//  Created by Apprenant 154 on 03/06/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ WearRU/WearRUApp.swift
@@ -0,0 +1,32 @@
+//
+//  WearRUApp.swift
+//  WearRU
+//
+//  Created by Apprenant 154 on 03/06/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WearRUApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
