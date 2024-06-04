6fa6e6ae6cd2a2a5275f2f4576ace74fdc15fd78
Bubullebizarre1981 <loic.jochmans@gmail.com>
2024-06-04 14:27:03 +0200

    Mise à Jour 1 SplashView

--- WearRU/Views/SplashView.swift
+++ WearRU/Views/SplashView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct SplashView: View {
+    @State private var isLoading: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            ZStack{
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.colorBackgroundDark)
+                    .frame(width: 230, height: 230)
+                RoundedRectangle(cornerRadius: 15)
+                    .fill(Color.colorBackgroundLight)
+                    .frame (width: 210, height: 210)
+                Image("Logo")
+                    .resizable()
+                    .frame (width: 210, height: 210)
+            }
+            .padding(.bottom, 100)
+            if isLoading{
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle(tint: .accentColor))
+                    .scaleEffect(7)
+            }
+        }
+        .onAppear { loadingCircleSplashView() }
+    }
+    func loadingCircleSplashView() {
+        isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+            isLoading = false
+        }
     }
 }
 
