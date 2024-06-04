ba2bfcde6fcd905de4811b72ef5052528bcd7baa
Johan Q. <johan.quille@protojo.fr>
2024-06-04 14:33:52 +0200

    Updates Images Shop

--- WearRU/Views/ExplorerListView.swift
+++ WearRU/Views/ExplorerListView.swift
@@ -22,6 +22,14 @@ struct ExplorerListView: View {
             VStack {
                 CardShop(image: "CardShop_1", title: "Erica Zhou", location: "Westfield Rosny 2", phone: "01 56 67 87 98", hours: "Lun-Sam: 10:00 – 20:30 \nDim: 10:00 – 19:00")
                 CardShop(image: "CardShop_2", title: "Les.Basics", location: "Westfield Rosny 2", phone: "01 03 87 27 98", hours: "Lun-Sam: 10:00 – 20:30 \nDim: 10:00 – 19:00")
+                CardShop(image: "CardShop_3", title: "Les.Basics", location: "Westfield Rosny 2", phone: "01 03 87 27 98", hours: "Lun-Sam: 10:00 – 20:30 \nDim: 10:00 – 19:00")
+                CardShop(image: "CardShop_4", title: "Les.Basics", location: "Westfield Rosny 2", phone: "01 03 87 27 98", hours: "Lun-Sam: 10:00 – 20:30 \nDim: 10:00 – 19:00")
+                CardShop(image: "CardShop_5", title: "Les.Basics", location: "Westfield Rosny 2", phone: "01 03 87 27 98", hours: "Lun-Sam: 10:00 – 20:30 \nDim: 10:00 – 19:00")
+                CardShop(image: "CardShop_6", title: "Les.Basics", location: "Westfield Rosny 2", phone: "01 03 87 27 98", hours: "Lun-Sam: 10:00 – 20:30 \nDim: 10:00 – 19:00")
+                CardShop(image: "CardShop_7", title: "Les.Basics", location: "Westfield Rosny 2", phone: "01 03 87 27 98", hours: "Lun-Sam: 10:00 – 20:30 \nDim: 10:00 – 19:00")
+                CardShop(image: "CardShop_8", title: "Les.Basics", location: "Westfield Rosny 2", phone: "01 03 87 27 98", hours: "Lun-Sam: 10:00 – 20:30 \nDim: 10:00 – 19:00")
+                CardShop(image: "CardShop_9", title: "Les.Basics", location: "Westfield Rosny 2", phone: "01 03 87 27 98", hours: "Lun-Sam: 10:00 – 20:30 \nDim: 10:00 – 19:00")
+                CardShop(image: "CardShop_10", title: "Les.Basics", location: "Westfield Rosny 2", phone: "01 03 87 27 98", hours: "Lun-Sam: 10:00 – 20:30 \nDim: 10:00 – 19:00")
             }
         }
     }
