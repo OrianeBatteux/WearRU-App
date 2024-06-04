21ca98807e31aabcaf6a9767ecfad11808ac274b
Bubullebizarre1981 <loic.jochmans@gmail.com>
2024-06-04 15:02:17 +0200

    Mise à jour 2 SplashView

--- WearRU/Views/SplashView.swift
+++ WearRU/Views/SplashView.swift
@@ -10,26 +10,33 @@ import SwiftUI
 struct SplashView: View {
     @State private var isLoading: Bool = false
     var body: some View {
-        VStack{
-            ZStack{
-                RoundedRectangle(cornerRadius: 20)
-                    .fill(Color.colorBackgroundDark)
-                    .frame(width: 230, height: 230)
-                RoundedRectangle(cornerRadius: 15)
-                    .fill(Color.colorBackgroundLight)
-                    .frame (width: 210, height: 210)
-                Image("Logo")
-                    .resizable()
-                    .frame (width: 210, height: 210)
-            }
-            .padding(.bottom, 100)
-            if isLoading{
-                ProgressView()
-                    .progressViewStyle(CircularProgressViewStyle(tint: .accentColor))
-                    .scaleEffect(7)
-            }
+        ZStack {
+            Color(.colorBackgroundLight)
+                .ignoresSafeArea()
+            VStack{
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color.colorBackgroundDark)
+                            .frame(width: 230, height: 230)
+                        RoundedRectangle(cornerRadius: 15)
+                            .fill(Color.colorBackgroundLight)
+                            .frame (width: 210, height: 210)
+                        Image("Logo")
+                            .resizable()
+                            .frame (width: 200, height: 180)
+                        VStack{
+                            if isLoading{
+                                Spacer()
+                                ProgressView()
+                                    .progressViewStyle(CircularProgressViewStyle(tint: .accentColor))
+                                    .scaleEffect(7)
+                            }
+                        }
+                    }
+                    .padding(.bottom, 100)
+                }
+            .onAppear { loadingCircleSplashView() }
         }
-        .onAppear { loadingCircleSplashView() }
     }
     func loadingCircleSplashView() {
         isLoading = true
