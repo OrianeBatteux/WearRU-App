0189c826874ef90ea13e1b02195fa76eb47f876a
Johan Q. <johan.quille@protojo.fr>
2024-06-04 09:40:30 +0200

    Update ExplorerListView.swift

--- WearRU/Views/ExplorerListView.swift
+++ WearRU/Views/ExplorerListView.swift
@@ -25,8 +25,37 @@ struct ExplorerListView: View {
                     .frame(height: 20)
                     .foregroundStyle(Color(.green))
             }
-            .foregroundStyle(Color(.colorText))
+            .foregroundStyle(.colorText)
             .padding()
+            HStack {
+                Image(systemName: "mappin.circle.fill")
+                    .foregroundStyle(Color(.colorPrimary))
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Text("Westfield Rosny 2")
+                    .foregroundStyle(.colorText)
+                Image(systemName: "phone.circle.fill")
+                    .foregroundStyle(Color(.colorPrimary))
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Text("01 48 12 62 60")
+                    .foregroundStyle(.colorText)
+            }
+            VStack {
+                HStack {
+                    Image(systemName: "clock.circle.fill")
+                        .foregroundStyle(Color(.colorPrimary))
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    Text("Horaires : Lun-Sam: 10:00 – 20:30 Dim: 10:00 – 19:00")
+                        .foregroundStyle(.colorText)
+                    Image(systemName: "map.circle.fill")
+                        .foregroundStyle(Color(.colorPrimary))
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    Text("Itinéraire ")
+                        .foregroundStyle(.colorText)
+                    Image(systemName: "arrowshape.forward.circle")
+                        .foregroundStyle(Color(.colorPrimary))
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                }
+            }
         }
     }
 }
