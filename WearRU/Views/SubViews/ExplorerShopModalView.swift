cfa3f6f5fb18a4a2e0e0fabcde7e9a329cc04d04
OrianeBatteux <oriane08@icloud.com>
2024-06-05 16:42:20 +0200

    Start Modal

--- WearRU/Views/SubViews/ExplorerShopModalView.swift
+++ WearRU/Views/SubViews/ExplorerShopModalView.swift
@@ -2,14 +2,34 @@
 //  ExplorerShopModalView.swift
 //  WearRU
 //
-//  Created by Apprenant 171 on 03/06/2024.
+//  Created by Apprenant Oriane on 03/06/2024.
 //
 
 import SwiftUI
 
 struct ExplorerShopModalView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Rectangle()
+                .fill(Color("Color-BackgroundLight"))
+                .frame(width: 1000, height: 1000)
+            VStack{
+                Text("Aushopping Val de Fontenay")
+                HStack{
+                    Image("CardShop_1")
+                        .resizable()
+                        .frame(width: 150, height: 100 )
+                    Image("CardShop_1")
+                        .resizable()
+                        .frame(width: 150, height: 100 )
+                }
+                    VStack{
+                        Text("Horaire: Ouvert . Ferme à 20:00")
+                        
+                        Text("Centre commercial Aushopping, 94120 Fontnay-sous-Bois")
+                    }
+            }
+        }
     }
 }
 
