c9a5399232568fde2c6934510efa64b3e71999b0
OrianeBatteux <oriane08@icloud.com>
2024-07-02 16:40:11 +0200

    Updates

--- WearRU/Components/PinExView.swift
+++ WearRU/Components/PinExView.swift
@@ -72,7 +72,7 @@ struct PinExView: View {
             .offset(y: pinSize * 0.3)
         })
         .sheet(isPresented: $isVisible) {
-        } content: { ExplorerShopModalView(selectedShop: shopForPin, isVisible: $isVisible, height: 0.95)
+        } content: { ExplorerShopModalView(selectedShop: shopForPin, isVisible: $isVisible, height: 0.65)
         }
         .animation(.spring, value: pinSize)
         .frame(width: pinSize * 1.3, height: pinSize * 2.8)
